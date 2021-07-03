e4b6165424626fa3c00c24aa50ea6a2607d35096
caner.demirci <caner.demirci@trendyol.com>
2021-07-03 23:54:03 +0300

    Create Multiple Target Library

---
+++ src/MultipleTargetLibrary/FrameworkHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MultipleTargetLibrary
+{
+    public class FrameworkHelper
+    {
+        public static FrameworkType GetFrameworkType()
+        {
+#if NET5_0
+            return FrameworkType.Net5;
+#elif NETCOREAPP3_1
+            return FrameworkType.NetCore3_1;
+#else
+            return FrameworkType.None;
+#endif
+        }
+    }
+}
---
+++ src/MultipleTargetLibrary/FrameworkType.cs
@@ -0,0 +1,9 @@
+namespace MultipleTargetLibrary
+{
+    public enum FrameworkType
+    {
+        None = 0,
+        NetCore3_1 = 1,
+        Net5 = 2
+    }
+}
